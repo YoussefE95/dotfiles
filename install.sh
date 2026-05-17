51f5cf80379872c669a4d163efab50ba33f7416f
Youssef <youssef95elzein@gmail.com>
2026-05-16 17:07:18 -0700

    Screensharing used gnome-portal, everything else kde-portal

--- install.sh
+++ install.sh
@@ -3,16 +3,13 @@
 paru -Syu && paru -S \
     sddm hyprland hyprlock hyprshot niri awww quickshell-git kitty starship \
     neovim ranger nvtop htop obsidian dropbox discord obs-studio \
-    spotify-launcher spicetify-cli dolphin dolphin-plugins okular \
-    gwenview ttf-jetbrains-mono ttf-jetbrains-mono-nerd brightnessctl \
-    jq python-pipx python-pillow docker docker-buildx ripgrep wl-clipboard \
-    gnome-keyring xdg-desktop-portal-kde polkit-kde-agent qt5-wayland \
-    qt6-wayland qt6ct zsh zsh-autosuggestions zsh-history-substring-search \
-    zsh-syntax-highlighting libappindicator-gtk3 archlinux-xdg-menu \
-    xwayland-satellite playerctl pavucontrol-qt systemsettings
-
-# Delete unnecessary folders
-sudo rm -rf ~/Documents ~/Music ~/Public ~/Videos ~/Templates ~/Pictures
+    spotify-launcher spicetify-cli dolphin dolphin-plugins okular gwenview \
+    ttf-jetbrains-mono ttf-jetbrains-mono-nerd brightnessctl jq python-pipx \
+    python-pillow docker docker-buildx ripgrep wl-clipboard gnome-keyring \
+    xdg-desktop-portal-gnome xdg-desktop-portal-kde polkit-kde-agent \
+    systemsettings qt5-wayland qt6-wayland qt6ct zsh zsh-autosuggestions \
+    zsh-history-substring-search zsh-syntax-highlighting libappindicator-gtk3 \
+    archlinux-xdg-menu xwayland-satellite playerctl pavucontrol-qt
 
 # Copy config files
 sudo rsync -a .config .local .zshrc ~/
@@ -39,12 +36,6 @@ sudo chown $USER:$USER \
     /usr/share/sddm/themes/dynamic/Main.qml \
     /usr/share/sddm/themes/dynamic/background.jpg
 
-# Set wallpaper
-awww img ~/.local/share/wallpaper.jpg \
-    --transition-type wipe \
-    --transition-angle 30 \
-    --transition-step 90
-
 # Set KDE theme
 lookandfeeltool -a org.kde.breezedark.desktop
 plasma-apply-colorscheme "DynamicTheme"
@@ -57,6 +48,15 @@ sudo gpasswd -a $USER docker
 
 # Make zsh default shell
 chsh -s $(which zsh)
+
+# Delete unnecessary folders
+sudo rm -rf ~/Documents ~/Music ~/Public ~/Videos ~/Templates ~/Pictures
+
+# Set wallpaper
+# awww img ~/.local/share/wallpaper.jpg \
+#     --transition-type wipe \
+#     --transition-angle 30 \
+#     --transition-step 90
 
 # Install Spicetify
 # spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 \
